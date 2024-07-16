ad51752673bc44369b758d89ec08e1a377be2ac3
Jasika Awasthi <jasikaawasthi@gmail.com>
2024-07-16 11:02:38 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,29 @@
+let display = document.getElementById('display');
+
+function clearDisplay() {
+    display.textContent = '0';
+}
+
+function appendToDisplay(value) {
+    if (display.textContent === '0') {
+        display.textContent = value;
+    } else {
+        display.textContent += value;
+    }
+}
+
+function deleteLast() {
+    if (display.textContent.length === 1) {
+        display.textContent = '0';
+    } else {
+        display.textContent = display.textContent.slice(0, -1);
+    }
+}
+
+function calculateResult() {
+    try {
+        display.textContent = eval(display.textContent);
+    } catch {
+        display.textContent = 'Error';
+    }
+}
